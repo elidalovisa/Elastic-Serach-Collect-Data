22dc33d872eb6febb4279533c7ed8b46abaca9d3
Elida Arrechea <elida@Elidas-MacBook-Air.local>
2022-04-22 17:04:33 +0200

    create index and save to es

--- index.mjs
+++ index.mjs
@@ -16,11 +16,8 @@ csv().fromFile(csvFilePath)
             row.id = uniqid() // Give each obj a unique ID
             entries.push(row)
         })
-        console.log(jsonObj)
+      //  console.log(jsonObj)
     })
-
-// Create index
-let dataIndex = ''
 
 // Create Elastic client
 const client = new Client({
@@ -29,7 +26,49 @@ const client = new Client({
         username: 'elastic',
         fingerprint: process.env.FINGERPRINT,
         password: process.env.PASSWORD
-    }
+    },
+    tls: {
+        rejectUnauthorized: false
+      }
 })
+/*
+try {
+    let result = await client.info()
+    console.log(result)
+} catch (error) {
+    console.log(error)
+}
+*/
+// Create index
+async function createIndex() {
+   await client.indices.create({
+        index: 'argentinadataTest',
+        operations: {
+            mappings: {
+              properties: {
+                province: { type: 'text' },
+                gdp: { type: 'integer' },
+                illiteracy: { type: 'integer' },
+                poverty: { type: 'integer' },
+                deficient_infra: { type: 'integer'},
+                school_dropout: { type: 'integer' },
+                no_healthcare: { type: 'integer' },
+                birth_mortal: { type: 'integer '},
+                pop: { type: 'integer' },
+                movie_theatres_per_cap: { type: ' integer' },
+                doctors_per_cap: { type: 'integer' },
+                id: { type: 'key' },
+              }
+            }
+          }
+        }, { ignore: [400] })
+    
+
+    const operations = entries.flatMap(doc => [{ index: { _index: 'argentinadata' } }, doc])
+    const bulkResponse = await client.bulk({ refresh: true, operations})
+console.log(bulkResponse.items[1])
+}
+
+createIndex()
 
 // Save data to ElasticSearch (BulkAll --> read doc)
