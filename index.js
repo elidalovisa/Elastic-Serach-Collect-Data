a9c74c5b4a6ea95ac74a4db883695fd6faf38781
Elida Arrechea <elida@Elidas-MacBook-Air.local>
2022-04-22 12:54:16 +0200

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+// Get data
+
+// Loop/sort data
+
+// Each document needs to have a unique id -> Create an ID
+
+// Create index
+let musicData = ''
+
+
+// Save data to ElasticSearch
+
+console.log('test')
